a8dd768d56a16b853e1b836545afe482f7a60898
Jaifar <tetiana.kravchuk1608@gmail.com>
2016-05-26 19:31:41 -0400

    Создан пакет для моей первой программы

--- Sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -2,8 +2,8 @@ package ru.stqa.ptf.sandbox;
 
 public class MyFirstProgram {
 
-  public static void main(String[] arg) {
-System.out.println("Hello, world");
- }
+    public static void main(String[] arg) {
+        System.out.println("Hello, world");
+    }
 
 }
