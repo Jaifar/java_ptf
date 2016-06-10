834e6b017dfd549a1afdd4b7cb3bd3341535a769
Jaifar <tetiana.kravchuk1608@gmail.com>
2016-06-09 20:46:19 -0400

    New Methods in creating a new contact

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/newContactData.java
@@ -0,0 +1,85 @@
+package ru.stqa.pft;
+
+public class newContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String home;
+  private final String mobile;
+  private final String work;
+  private final String email;
+  private final String byear;
+  private final String ayear;
+
+  public newContactData(String firstname, String middlename, String lastname, String nickname, String title, String company, String address, String home, String mobile, String work, String email, String byear, String ayear) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.home = home;
+    this.mobile = mobile;
+    this.work = work;
+    this.email = email;
+    this.byear = byear;
+    this.ayear = ayear;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHome() {
+    return home;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getWork() {
+    return work;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getByear() {
+    return byear;
+  }
+
+  public String getAyear() {
+    return ayear;
+  }
+}
