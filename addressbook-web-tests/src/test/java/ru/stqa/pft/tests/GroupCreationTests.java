b2c627389a70dc49c2e27097465b536b60a80e19
Jaifar <tetiana.kravchuk1608@gmail.com>
2016-07-06 19:00:50 -0400

    Comparator

--- addressbook-web-tests/src/test/java/ru/stqa/pft/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/tests/GroupCreationTests.java
@@ -29,21 +29,10 @@ public class GroupCreationTests extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    Comparator<? super GroupData> byId = new Comparator<GroupData>() {
-      @Override
-      public int compare(GroupData o1, GroupData o2) {
-        return Integer.compare(o1.getId(), o2.getId());
-      }
-    };
-    int max1 = after.stream().max(byId).get().getId();
-    group.setId(max1);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }}
