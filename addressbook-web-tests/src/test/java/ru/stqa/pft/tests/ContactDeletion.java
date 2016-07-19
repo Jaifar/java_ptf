bc5a5d3b5b81a0e8bc91ce9b926b9f1b303323fd
Jaifar <tetiana.kravchuk1608@gmail.com>
2016-07-19 12:23:51 -0400

    Задание 9: Test Contact modification and Contact deletion run well

--- addressbook-web-tests/src/test/java/ru/stqa/pft/tests/ContactDeletion.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/tests/ContactDeletion.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.model.ContactData;
 
 public class ContactDeletion  extends TestBase2{
 
@@ -12,6 +13,11 @@ public class ContactDeletion  extends TestBase2{
     
     @Test
     public void testContactDeletion() {
+        app.getNavigationHelperContact().returnsToHomePage();
+
+        if (! app.getContactHelper().isThereAContact()){
+            app.getContactHelper().createGroup(new ContactData("Tetiana", "V", "Kravchuk", "Jaifar", "Me", "Informatrix Global", "Los Altos, California", "345-987-9876", "978-678-9876", "489-983-9874", "teti@gmail.com"));
+        }
 
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteContact();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/tests/ContactModification.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/tests/ContactModification.java
@@ -10,6 +10,11 @@ public class ContactModification extends TestBase2 {
 
   @Test
   public void testContactModification(){
+    app.getNavigationHelperContact().returnsToHomePage();
+
+    if (! app.getContactHelper().isThereAContact()){
+      app.getContactHelper().createGroup(new ContactData("Tetiana", "V", "Kravchuk", "Jaifar", "Me", "Informatrix Global", "Los Altos, California", "345-987-9876", "978-678-9876", "489-983-9874", "teti@gmail.com"));
+    }
     app.getContactHelper().selectContactMofication();
     app.getContactHelper().updateContactForm();
 
