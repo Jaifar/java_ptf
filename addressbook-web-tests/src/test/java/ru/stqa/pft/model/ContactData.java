33cc4122f783a2d3ca6ed749791f43286874ecc6
Jaifar <tetiana.kravchuk1608@gmail.com>
2016-07-26 22:26:29 -0400

    4.10. Test Creation. Тест работает.1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/model/ContactData.java
@@ -12,7 +12,7 @@ public class ContactData {
 
 
   public ContactData(String firstname, String middlename, String lastname, String nickname, String group) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.firstname = firstname;
     this.middlename = middlename;
     this.lastname = lastname;
