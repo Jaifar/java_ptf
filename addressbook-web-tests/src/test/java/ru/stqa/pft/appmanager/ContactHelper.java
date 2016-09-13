a66eff91273cae347a8ec3ded148300fdd1eac7f
Jaifar <tetiana.kravchuk1608@gmail.com>
2016-09-13 17:34:58 -0400

    5.10. Режем строки. Тест не проходит.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/appmanager/ContactHelper.java
@@ -74,12 +74,13 @@ public class ContactHelper extends ContactHelperBase {
   public ContactData infoFromEditForm(ContactData contact) {
 
     initContactModificationById(contact.getId());
-    initContactModification(contact.getId());
+
     String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
     String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
     String home = wd.findElement(By.name("home")).getAttribute("value");
     String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
     String work = wd.findElement(By.name("work")).getAttribute("value");
+
     wd.navigate().back();
     return new ContactData().withId(contact.getId()).withFirstname(firstname).withLastname(lastname)
             .withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work);
@@ -87,10 +88,12 @@ public class ContactHelper extends ContactHelperBase {
   }
 
   private void initContactModificationById(int id) {
-    WebElement checkbox = wd.findElement(By.cssSelector(String.format("input[value='%s']",id)));
+    WebElement checkbox = wd.findElement(By.cssSelector(String.format("input[value='%s']", id)));
     WebElement row = checkbox.findElement(By.xpath("./../.."));
     List<WebElement> cells = row.findElements(By.tagName("td"));
     cells.get(7).findElement(By.tagName("a")).click();
+
+   // wd.findElement(By.cssSelector(String.format("a[href='edit.php?id=%s']", id))).click();
   }
 
   public void initContactModification(int id) {
@@ -175,17 +178,13 @@ public class ContactHelper extends ContactHelperBase {
       List<WebElement> cells = row.findElements(By.tagName("td"));
       int id = Integer.parseInt(row.findElement(By.tagName("input")).getAttribute("value"));
       String lastname = cells.get(1).getText();
-
       String firstname = cells.get(2).getText();
-      String address = cells.get(3).getText();
-      String AllEmails = cells.get(4).getText();
-
-      String[] phones = cells.get(5).getText().split("\n");
+      String[] phones =  cells.get(5).getText().split("\n");
 
 
-      contacts.add(new ContactData().withId(id).withLastname(lastname)
-              .withFirstname(firstname).withAlladdress(address)
-              .withAllemails(AllEmails)
+
+
+      contacts.add(new ContactData().withId(id).withLastname(lastname).withFirstname(firstname)
               .withHomePhone(phones[0]).withMobilePhone(phones[1]).withWorkPhone(phones[2]));
     }
     return contacts;
